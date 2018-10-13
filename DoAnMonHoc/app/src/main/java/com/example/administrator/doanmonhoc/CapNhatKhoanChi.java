b1b65a15413ff7be826284cf34b4d687a8eaae01
Doan Nguyen Duy Phuong <phuongdnd1997@gmail.com>
2018-10-13 20:10:51 +0700

    My First Commit

---
+++ DoAnMonHoc/app/src/main/java/com/example/administrator/doanmonhoc/CapNhatKhoanChi.java
@@ -0,0 +1,13 @@
+package com.example.administrator.doanmonhoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CapNhatKhoanChi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cap_nhat_khoan_chi);
+    }
+}
---
+++ DoAnMonHoc/app/src/main/java/com/example/administrator/doanmonhoc/ThemKhoanChi.java
@@ -0,0 +1,13 @@
+package com.example.administrator.doanmonhoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThemKhoanChi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.them_khoan_chi);
+    }
+}
